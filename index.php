ba0f655d98b8e5eb672d27a774da5f38b6b567b5
Spudwebb96 <jonas.janko17@gmail.com>
2022-02-02 17:17:40 +0100

    update v4

--- index.php
+++ index.php
@@ -3,31 +3,32 @@ require_once 'db.php';
 require_once 'stuff.php';
 
 $inst = new DB();
-$update = new update();
+$create = new create();
 
 $query = $inst->db->query('SELECT * FROM stats');
 $response = $query->fetchall(\PDO::FETCH_ASSOC);
 
 var_dump($_GET, $_POST);
 
-// if (isset($_POST['update'])){
-//     $update->addrow($inst);
 
-// };
 
-if (isset($_POST['update'])){
-    $update->addrowm($inst,$_POST['firstname'],$_POST['lastname'],$_POST['ppg'],$_POST['rpg'],$_POST['apg'],$_POST['spg'],$_POST['tpg'],$_POST['team']);
+if (isset($_POST['create'])){
+    $create->addrowm($inst,$_POST['firstname'],$_POST['lastname'],$_POST['ppg'],$_POST['rpg'],$_POST['apg'],$_POST['spg'],$_POST['tpg'],$_POST['team']);
     header("Location: index.php");
     exit;
 };
 
+if (isset($_POST['update'])){
+    $create->updaterow($id);
+}
+
 ?>
-<!-- <a href="index.php?update=true">Click me</a>
+<!-- <a href="index.php?create=true">Click me</a>
 <a href="test.php">Click me</a> -->
 
 <form action="index.php" method="post" target="_self">
 
-    <input type="hidden" name="update" value="true">
+    <input type="hidden" name="create" value="true">
     <input type="text" name="firstname" value="Mike">    
     <input type="text" name="lastname" value="Jordan">
     <input type="number" step="0.01" name="ppg" value="23">
@@ -42,13 +43,21 @@ if (isset($_POST['update'])){
 
 <table>
 
+
+
 <?php foreach ($response as $key => $row) { ?>
-    <tr> 
+    <tr> <?php var_dump($row)?>
     <?php foreach ($row as $innerkey => $column){ ?>
         <td>
-            <?php echo 'row :' . $key . ' -> ' . $innerkey . '=>' . $column; ?>
+            <?php echo 'row :' . $key . ' -> ' . $innerkey . '=>' . $column;?>
         </td>
-    <?php } ?> </tr>
+    <?php } ?> <td>
+        <form action="index.php" method="post" target="_self">
+            <input type="hidden" name="update" value="<?php echo $row["id"]?>">
+            <button>Edit</button> 
+        </form> 
+    </td>
     </tr>
 <?php } ?> 
 </table>
+
--- stuff.php
+++ stuff.php
@@ -8,32 +8,149 @@ require_once 'db.php';
 
 // var_dump($stmt);
 
-class update
+class create
 {
-    public function addrow($db)
-    {
-        $sql = "INSERT INTO `stuff` (`a`, `b`, `c`, `d`, `e`, `f`, `g`, `h`) VALUES ('asd', '1', 'asd', '1', '1', 'asd', '1', '1')";
-        $stmt = $db->db->prepare("INSERT INTO `stuff` (`a`, `b`, `c`, `d`, `e`, `f`, `g`, `h`) VALUES ('asd', '1', 'asd', '1', '1', 'asd', '1', '1')");
-        $stmt->execute();        
-    }
+    // public function addrow($db)
+    // {
+    //     $sql = "INSERT INTO `stuff` (`a`, `b`, `c`, `d`, `e`, `f`, `g`, `h`) VALUES ('asd', '1', 'asd', '1', '1', 'asd', '1', '1')";
+    //     $stmt = $db->db->prepare("INSERT INTO `stuff` (`a`, `b`, `c`, `d`, `e`, `f`, `g`, `h`) VALUES ('asd', '1', 'asd', '1', '1', 'asd', '1', '1')");
+    //     $stmt->execute();        
+    // }
 
     public function addrowm($db,$a,$b,$c,$d,$e,$f,$g,$h)
     {
-        $sql = "INSERT INTO stuff ('First Name','Last Name','PPG','RPG','APG','SPG','TPG','Team') 
-        VALUES (':First Name',':Last Name',:PPG,:RPG,:APG,:SPG,:TPG,:Team)";
+        $sql = "INSERT INTO stats (firstname,lastname,ppg,rpg,apg,spg,tpg,team) 
+        VALUES (:firstname,:lastname,:ppg,:rpg,:apg,:spg,:tpg,:team)";
         $stmt = $db->db->prepare($sql);
 
-        $stmt->bindParam(':First Name', $a, \PDO::PARAM_STR);
-        $stmt->bindParam(':Last Name', $b, \PDO::PARAM_STR);
-        $stmt->bindParam(':PPG', $c, \PDO::PARAM_INT);
-        $stmt->bindParam(':RPG', $d, \PDO::PARAM_INT);
-        $stmt->bindParam(':APG', $e, \PDO::PARAM_INT);
-        $stmt->bindParam(':SPG', $f, \PDO::PARAM_INT);
-        $stmt->bindParam(':TPG', $g, \PDO::PARAM_INT);
-        $stmt->bindParam(':Team', $h, \PDO::PARAM_STR);
+        $stmt->bindParam(':firstname', $a, \PDO::PARAM_STR);
+        $stmt->bindParam(':lastname', $b, \PDO::PARAM_STR);
+        $stmt->bindParam(':ppg', $c, \PDO::PARAM_INT);
+        $stmt->bindParam(':rpg', $d, \PDO::PARAM_INT);
+        $stmt->bindParam(':apg', $e, \PDO::PARAM_INT);
+        $stmt->bindParam(':spg', $f, \PDO::PARAM_INT);
+        $stmt->bindParam(':tpg', $g, \PDO::PARAM_INT);
+        $stmt->bindParam(':team', $h, \PDO::PARAM_STR);
 
         $stmt->execute();  
         echo $sql;      
+    }
+
+    public function updaterow($id)
+    {
+        $stmt = $this->db->prepare("UPDATE stats SET firstname = :firstname, lastname = :lastname, ppg = :ppg,
+         rpg = :rpg, apg = :apg, spg = :spg, tpg = :tpg, team = :team WHERE id = :id");
+
+        $firstname = $stats->getfirstname();
+        $lastname = $stats->getlastname();
+        $ppg = $stats->getppg();
+        $rpg = $stats->getrpg();
+        $apg = $stats->getapg();
+        $spg = $stats->getspg();
+        $tpg = $stats->gettpg();
+        $team = $stats->getteam();
+
+        $stmt->bindParam(':firstname', $firstname, \PDO::PARAM_STR);
+        $stmt->bindParam(':lastname', $lastname, \PDO::PARAM_STR);
+        $stmt->bindParam(':ppg', $ppg, \PDO::PARAM_INT);
+        $stmt->bindParam(':rpg', $rpg, \PDO::PARAM_INT);
+        $stmt->bindParam(':apg', $apg, \PDO::PARAM_INT);
+        $stmt->bindParam(':spg', $spg, \PDO::PARAM_INT);
+        $stmt->bindParam(':tpg', $tpg, \PDO::PARAM_INT);
+        $stmt->bindParam(':team', $team, \PDO::PARAM_STR);
+        $stmt->bindParam(':id', $id, \PDO::PARAM_INT);
+
+        $stmt->execute();  
+    }
+
+    public function getfirstname(): string
+    {
+        return $this->firstname;
+    }
+
+    public function setfirstname(string $firstname): void
+    {
+        $this->firstname = $firstname;
+    }
+
+    public function getlastname(): string
+    {
+        return $this->lastname;
+    }
+
+    public function setlastname(string $lastname): void
+    {
+        $this->lastname = $lastname;
+    }
+
+    public function getppg(): string
+    {
+        return $this->ppg;
+    }
+
+    public function setppg(string $ppg): void
+    {
+        $this->ppg = $ppg;
+    }
+
+    public function getrpg(): string
+    {
+        return $this->rpg;
+    }
+
+    public function setrpg(string $rpg): void
+    {
+        $this->rpg = $rpg;
+    }
+
+    public function getapg(): string
+    {
+        return $this->apg;
+    }
+
+    public function setapg(string $apg): void
+    {
+        $this->apg = $apg;
+    }
+
+    public function getspg(): string
+    {
+        return $this->spg;
+    }
+
+    public function setspg(string $spg): void
+    {
+        $this->spg = $spg;
+    }
+
+    public function gettpg(): string
+    {
+        return $this->tpg;
+    }
+
+    public function settpg(string $tpg): void
+    {
+        $this->tpg = $tpg;
+    }
+
+    public function getteam(): string
+    {
+        return $this->team;
+    }
+
+    public function setteam(string $team): void
+    {
+        $this->team = $team;
+    }
+    
+    public function getid(): string
+    {
+        return $this->id;
+    }
+
+    public function setid(string $id): void
+    {
+        $this->id = $id;
     }
 }
 ?>
